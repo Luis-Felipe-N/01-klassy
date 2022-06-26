1c433ab521427b85012b630d0f1170800be622ac
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-06-26 18:24:34 -0300

    fix files js

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 import './src/css/style.css'
+const btnMenu = document.querySelector(".btnMenu")
+const header = document.querySelector(".header nav")
+
+btnMenu.addEventListener('click', () => {
+    header.classList.toggle("active")
+})
+
+
+const containerSlider = document.querySelector('.hero__slider')
+const sliders = containerSlider.querySelectorAll('li')
+const btnBack = document.querySelector('.btn__slider_left')
+const btnNext = document.querySelector('.btn__slider_right')
+
+let index = 0;
+
+function moveSlider() {
+  containerSlider.style.transform = `translateX(-${index * 100}%)`
+}
+
+btnBack.addEventListener('click', () => {
+  if (index == 0) {
+    index = sliders.length -1
+  } else {
+    index--
+  }
+  moveSlider()
+})
+
+btnNext.addEventListener('click', () => {
+  if (index == sliders.length - 1) {
+    index = 0
+  } else {
+    index += 1
+  }
+  moveSlider()
+})
